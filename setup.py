2274ba6e17c139f50d98899c126eaf05ea671b92
woodcrafty <mark.l.a.richards@gmail.com>
2015-03-05 16:40:57 +0000

    Minor changes

--- setup.py
+++ setup.py
@@ -1,23 +1,17 @@
 
-from setuptools import setup, find_packages
-
-#import record
+from setuptools import setup
 
 setup(
     name='record',
-    #version=record.__version__,
-    version='0.1.0',
+    version='0.1',
     description=(
-        'Similar to namedtuple, but field values are mutable and there is no '
-        'limit on the number of fields'
+        'Easily create lightweight custom record classes'
     ),
     long_description=open('README.rst').read(),
     author='Mark Richards',
     author_email='mark.l.a.richardsREMOVETHIS@gmail.com',
     license='BSD 3-clause',
-    #packages=find_packages(),
-    #packages=['record'],
-    py_modules=['record', 'test_record'],
+    py_modules=['record'],
     package_data={'': ['*.rst', '*.txt']},
     classifiers=[
         'Programming Language :: Python',
